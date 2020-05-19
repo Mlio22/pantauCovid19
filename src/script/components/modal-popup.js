3bc64e6d1185a7cb75e55e85664d66db9dcb9735
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2020-05-19 07:32:00 +0700

    fixed dependencies

--- src/script/components/modal-popup.js
+++ src/script/components/modal-popup.js
@@ -1,6 +1,6 @@
 import countries from "../data/global-countries.js";
 import indonesiaProvinces from "../data/indonesia-provinces.js";
-import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js";
+import "../../../node_modules/jquery/dist/jquery.min.js";
 import "../../../node_modules/select2/dist/js/select2.min.js";
 
 class ModalPopup extends HTMLElement {
